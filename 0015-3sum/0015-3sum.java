7825d698e3bb61fe6d990166320f80fab75c28f7
Jayasurya Pazhani <jsurya733@gmail.com>
2026-09-20 16:58:55 -0400

    Time: 32 ms (75.83%), Space: 60.3 MB (9.18%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,43 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        Arrays.sort(nums);
+
+        List<List<Integer>> allPairs = new ArrayList<>();
+        for(int i =0;i<nums.length-2;i++){
+            int left = i+1;
+            int right = nums.length-1;
+
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+            while(left<right){
+                int sum = nums[i] + nums [left] + nums [right];
+
+                if(sum ==0){
+                    List<Integer> pairs = new ArrayList<>();
+                    pairs.add(nums[i]);
+                    pairs.add(nums[left]);
+                    pairs.add(nums[right]);
+                    allPairs.add(pairs);
+                    left++;
+                    right--;
+
+                    while(left<right && nums[left]==nums[left-1]){
+                        left++;
+                    }
+                    while(left<right && nums[right]==nums[right+1]){
+                        right--;
+                    }
+
+                }else if(sum<0){
+                    left++;
+                }else{
+                    right--;
+                }
+            }
+        }
+        return allPairs;
+        
+    }
+}
