64d63c9c6f0d8312f9455bb6fa4662cb8ba6c2e0
Jayasurya Pazhani <jsurya733@gmail.com>
2026-09-21 16:48:21 -0400

    Time: 0 ms (100%), Space: 48.4 MB (14.35%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,23 +1,46 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
 
-        int[] res = {-1,-1};
-        if(nums.length<1){
-            return res;
-        }
-        for(int i =0;i<nums.length;i++){
-            if(nums[i]==target){
-                res[0]=i;
-                break;
-            }
-        }
-        for(int i =nums.length-1;i>=0;i--){
-            if(nums[i]==target){
-                res[1]=i;
-                break;
-            }
-        }
-        return res;
+        int first = findFirst(nums,target);
+        int last = findLast(nums,target);
+
+        return new int[]{first,last};
+
         
+    }
+
+    static int findFirst(int[]nums, int target){
+        int result =-1;
+        int left =0;
+        int right = nums.length-1;
+        while(left<=right){
+            int mid = left + (right -left)/2;
+            if(nums[mid]==target){
+                result = mid;
+                right = mid-1;
+            }else if (nums[mid]>target){
+                right = mid -1;
+            }else{
+                left = mid +1;
+            }
+        }
+        return result;
+    }
+        static int findLast(int[]nums, int target){
+        int result =-1;
+        int left =0;
+        int right = nums.length-1;
+        while(left<=right){
+            int mid = left + (right -left)/2;
+            if(nums[mid]==target){
+                result = mid;
+                left = mid+1;
+            }else if (nums[mid]>target){
+                right = mid -1;
+            }else{
+                left = mid +1;
+            }
+        }
+        return result;
     }
 }
